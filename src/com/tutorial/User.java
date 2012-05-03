35a14057d2bb21d39ecc21fe7a57286a99b3beef
John Henrique Teixeira de Godoi <john@newshell.(none)>
2012-05-03 15:36:04 -0300

    refactoring Login e User beans

--- src/com/tutorial/User.java
+++ src/com/tutorial/User.java
@@ -1,6 +1,14 @@
+/**
+ * LoginBean.java
+ * 
+ */
+
 package com.tutorial;
 
-public class User {
+public class User
+{
+    private String name;
+    private String password;
 	private Integer id;
 
 	public void setId(Integer integer) {
@@ -8,4 +16,32 @@ public class User {
 
 	}
 
+
+    public Integer getId() {
+		return id;
+	}
+
+
+	public String getName ()
+    {
+        return name;
+    }
+
+
+    public void setName (final String name)
+    {
+        this.name = name;
+    }
+
+
+    public String getPassword ()
+    {
+        return password;
+    }
+
+
+    public void setPassword (final String password)
+    {
+        this.password = password;
+    }
 }
